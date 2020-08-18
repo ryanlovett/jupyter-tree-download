8ef5ff15973c3048020627c7235a29e67c9f8739
ryanlovett <rylo@berkeley.edu>
2020-08-18 10:13:17 -0700

    Partially revert #2. Remove tabs.

--- jupyter_tree_download/static/tree.js
+++ jupyter_tree_download/static/tree.js
@@ -11,14 +11,15 @@ define([
           $('<div>').addClass('btn-group').attr('id', 'tree-download').prepend(
                '<button class="btn btn-xs btn-default" title="Download Directory">Download Directory</button>'
           ).click(function() {
-            var dirPath = utils.get_body_data('notebookPath')
-			console.log("dir_path: " + dirPath);
+            //var dirPath = utils.get_body_data('notebookPath')
+            var dirPath = document.body.getAttribute('data-notebook-path');
+            console.log("dir_path: " + dirPath);
 
             var baseUrl = utils.get_body_data('baseUrl')
 
-			/* we name the file based on what the client sees as the hostname
+            /* we name the file based on what the client sees as the hostname
              * because it is usually a jupyterhub */
-			var hostname = document.location.hostname;
+            var hostname = document.location.hostname;
             window.location.href = baseUrl + 'tree-download?name=' + hostname + '&path=' + dirPath;
           })
         )
